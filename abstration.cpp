9d1d1afa6e97f1045dca07e9b01f1c588fee5f09
Denay2468 <denayaung13@gmail.com>
2026-05-19 10:02:45 +0630

    method to fill private member values

---
+++ abstration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class abrstraction
+{
+private:
+    string x, y;
+
+public:
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+};
